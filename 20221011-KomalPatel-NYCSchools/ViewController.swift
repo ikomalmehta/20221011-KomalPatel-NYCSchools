f7da2c0fb43ea14f631b848a11cd63e32a36864e
ikomalmehta <ikomalmehta@gmail.com>
2022-10-11 11:58:30 -0500

    Initial Commit

---
+++ 20221011-KomalPatel-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20221011-KomalPatel-NYCSchools
+//
+//  Created by komal patel on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
